50ce1af57476d577b8d23997f4e81adb64527c67
denis1729 <denis_cc1729@hotmail.com>
2022-03-16 23:39:40 -0400

    modify runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,11 +6,11 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        glue = {"src.test.java"},
+        glue = {"steps","hooks"},
         plugin = {"json:cucumber/bandeja_editar.json",
                 "pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        tags = "@buy_product")
+        tags = "@login or @buy_product")
 
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,25 +1,24 @@
 package steps;
 
 import config.UsersConfigReader;
-
 import entities.User;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.log4j.Logger;
+import ui.HomePage;
 import ui.LoginPage;
 import ui.PageTransporter;
 import ui.ProfilePage;
-import ui.HomePage;
 
 import java.net.MalformedURLException;
 
 import static org.testng.Assert.assertTrue;
 
-public class loginSteps {
-    private PageTransporter pageTransporter;
-    private Logger log = Logger.getLogger(getClass());
+public class LoginSteps {
+    private final PageTransporter pageTransporter;
+    private final Logger log = Logger.getLogger(getClass());
 
     //Pages
     private LoginPage loginPage;
@@ -29,25 +28,16 @@ public class loginSteps {
     // Entities
     private User user;
 
-    public loginSteps(User user) {
+    public LoginSteps(User user) {
         this.pageTransporter = new PageTransporter();
         this.user = user;
     }
 
-    //****************************************************************
-    //Login Step Definitions
-    //****************************************************************
     @Given("^I navigate to Login page$")
-    public void iNavigateToLoginPage() throws MalformedURLException {
+    public void i_navigate_to_login_page() throws MalformedURLException {
         //Use this step for login feature scenarios
         homePage = pageTransporter.navigateToHomePage();
         loginPage = homePage.goToLoginPage();
-    }
-
-    @When("^I login as \"(.*?)\" with password \"(.*?)\"$")
-    public void iLoginAsWithPassword(String email, String password) {
-        //Use this step for login feature scenarios
-        profilePage = loginPage.login(email, password);
     }
 
     @And("^I go to Home page$")
@@ -55,17 +45,22 @@ public class loginSteps {
         homePage = profilePage.goHomePage();
     }
 
-    @Then("^I should login successfully with a \"(.*?)\"$")
-    public void iShouldLoginSuccessfullyWithA(String fullName) {
-        String actual = profilePage.getUserName(fullName);
-        assertTrue(fullName.equals(actual), "full name the user is showed");
-
+    @When("^I login as \"(.*?)\" with password \"(.*?)\"$")
+    public void i_login_as_with_password(String email, String password) {
+        //Use this step for login feature scenarios
+        profilePage = loginPage.login(email, password);
     }
 
     @Given("^I (?:am logged in|login) as \"(.*?)\" User$")
     public void loginAsUser(final String userAlias) {
         user = UsersConfigReader.getInstance().getUserByAlias(userAlias);
-        iLoginAsWithPassword(user.getUserEmail(), user.getPassword());
+        i_login_as_with_password(user.getUserEmail(), user.getPassword());
+    }
+
+    @Then("^I should login successfully with a \"(.*?)\"$")
+    public void i_should_login_successfully_with_a(String fullName) {
+        String actual = profilePage.getUserName(fullName);
+        log.info(String.format("data %s and %s", fullName, actual));
+        assertTrue(fullName.equalsIgnoreCase(actual), "full name the user is showed");
     }
 }
-
