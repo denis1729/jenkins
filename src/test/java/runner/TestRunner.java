cf52ca62e98dcb39e56029870929de48e3480f84
denis1729 <denis_cc1729@hotmail.com>
2022-04-29 16:22:13 -0400

    test runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,21 +1,17 @@
 package runner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
-
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
         glue = {"steps","hooks"},
         plugin = {"json:cucumber/bandeja_editar.json",
                 "pretty",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        "html:target/cucumber","json:target/cucumber.json"},
         tags = "@login")
 
-public class TestRunner extends AbstractTestNGCucumberTests {
-    @Override
-    @DataProvider
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+public class TestRunner {
 }
